4a6a8f6a2e82baf5a8dc927f4ce8bb57432fbeaa
Voinea Radu <contact@voinearadu.com>
2024-04-13 19:36:19 +0300

    79/100

--- stochastic_text_gen/word_idx.m
+++ stochastic_text_gen/word_idx.m
@@ -1,4 +1,4 @@
-Aceasta functie intoarce un dictionar care contine indecsii asociati fiecarui label.
+%Aceasta functie intoarce un dictionar care contine indecsii asociati fiecarui label.
 function retval = word_idx (distinct_wds)
     % return the word_idx dictionary HINT: containers.Map
     retval = containers.Map(distinct_wds, 1:length(distinct_wds));
