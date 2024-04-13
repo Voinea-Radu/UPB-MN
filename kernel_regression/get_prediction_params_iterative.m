5dd569b386e73c11abfd8337559fd28abe7c7439
Voinea Radu <contact@voinearadu.com>
2024-04-13 19:23:54 +0300

    56/100

--- kernel_regression/get_prediction_params_iterative.m
+++ kernel_regression/get_prediction_params_iterative.m
@@ -1,24 +1,13 @@
 function [a] = get_prediction_params_iterative(K, y, lambda)
-  % Initialize a
-  a = zeros(size(K, 1), 1);
-  
-  % Conjugate gradient method
-  r = y - K * a;
-  p = r;
-  rsold = r' * r;
-  
-  for i = 1:length(y)
-      Kp = K * p;
-      alpha = rsold / (p' * Kp);
-      a = a + alpha * p;
-      r = r - alpha * Kp;
-      rsnew = r' * r;
-      
-      if sqrt(rsnew) < 1e-10
-            break;
-      end
-      
-      p = r + (rsnew / rsold) * p;
-      rsold = rsnew;
-  end
+  % Add a small lambda to the diagonal elements of the K matrix for regularization
+  K_reg = K + lambda * eye(size(K));
+
+  % Perform Cholesky decomposition
+  L = cholesky(K_reg);
+
+  % Solve for intermediate variable using forward substitution
+  v = L \ y;
+
+  % Solve for the parameter vector a using backward substitution
+  a = L.' \ v;
 end
