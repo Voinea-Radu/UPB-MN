87087cb93ce1313353cc906cf9975a6b62041596
Voinea Radu <contact@voinearadu.com>
2024-04-13 23:39:39 +0300

    100/100

--- checker/task-3/test-poe-stochastic-matrix/run_test.m
+++ checker/task-3/test-poe-stochastic-matrix/run_test.m
@@ -2,7 +2,7 @@
 function run_test()    
     fout = fopen("out", "w+");
     k = 2;
-    corpus = split_input("../../input/task3/data/small_100_poe.txt");  
+    corpus = split_input("../../input/task3/data/small_100_poe.txt");
     word_set = distinct_words(corpus);
     k_secv_corpus = k_secv(corpus, k);
     k_secv_set = distinct_k_secv(k_secv_corpus);
@@ -12,14 +12,13 @@ function run_test()
     kscvidx = k_secv_idx(k_secv_set);
     _kk = kscvidx.keys;
     _kv = kscvidx.values;
-    _stoch = stochastic_matrix(k_secv_corpus, corpus, word_set, k_secv_set, k);
 
-    stoch=stoch
-    _stoch=_stoch
-
+    _stoch = stochastic_matrix(k_secv_corpus, corpus, word_set, k_secv_set, k)
 
     save("ouput.mat", "_stoch", "_wk", "_wv", "_kk", "_kv")
     load("../../input/task3/ref/11_ref_poe_stochastic_matrix.mat")
+
+    stoch=stoch
 
     if isequal(stoch, _stoch)
         fprintf(fout, "1");
--- stochastic_text_gen/stochastic_matrix.m
+++ stochastic_text_gen/stochastic_matrix.m
@@ -1,28 +1,17 @@
-% Aceasta functie creeaza matricea stochastica, avand la dispozitie cele 2 dictionare de codificari, respectiv k-secventele în ordinea în care au fost date în "input.txt", respectiv token-urile (corpus\_words). K-secventei care incepe la pozitia $j$ ii va corespunde cuvantul care este la positia $j+k$ in input.txt.
 function retval = stochastic_matrix(k_secv_corpus, corpus_words, words_set, k_secv_set, k)
-    % build the stochastic matrix. Bonus if it is sparse
-    % The 'stochastic' matrix SHOULD NOT be scaled (divided by sum of all)
-    % It should just be entry-wise
-    % This is how the checker tests it, to not have to deal with floating point errors
-    % Initialize the stochastic matrix
-    stochastic_matrix = zeros(length(corpus_words) - k - 1, length(words_set));
+    stochastic_matrix = zeros(length(corpus_words) - k, length(words_set));
 
-    % Iterate through each k-secvent in the corpus
     for i = 1:length(k_secv_corpus)
-        % Get the index of the k-secvent in the k-secvent set
         k_secv_index = find(ismember(k_secv_set, k_secv_corpus{i}));
 
-        % Get the index of the next word in the corpus
         next_word_index = find(ismember(words_set, corpus_words{i+k}));
 
-        % Increment the corresponding entry in the stochastic matrix
-        stochastic_matrix(k_secv_index, next_word_index) = stochastic_matrix(k_secv_index, next_word_index) + 1;
+        stochastic_matrix(k_secv_index, next_word_index)=stochastic_matrix(k_secv_index, next_word_index) + 1;
     end
 
-    % Normalize the stochastic matrix
-    % stochastic_matrix = stochastic_matrix ./ sum(stochastic_matrix, 1);
+    % remove all zero rows at the end of the matrix
+    stochastic_matrix = stochastic_matrix(any(stochastic_matrix, 2), :);
 
-    % return the stochastic matrix as Compressed Column Sparse format
     retval = sparse(stochastic_matrix);
     %retval = stochastic_matrix;
 end
