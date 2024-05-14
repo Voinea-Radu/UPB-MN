a035c870834d47a53b57eac3d2d5a097a1001f23
Voinea Radu <contact@voinearadu.com>
2024-05-14 06:15:19 +0300

    Implemented reverb. Cleaned up stereo_to_mono. 30/100

--- src/numerical_music/apply_reverb.m
+++ src/numerical_music/apply_reverb.m
@@ -1,4 +1,10 @@
-function signal = apply_reverb(signal, impulse_response)
-  signal = 0;
-endfunction
+function output_signal = apply_reverb(signal, impulse_response)
+	if size(impulse_response, 2) != 1
+		impulse_response = mean(impulse_response, 2);
+	end
 
+	convolved_signal = fftconv(signal, impulse_response);
+
+	output_signal = convolved_signal / max(abs(convolved_signal));
+end
+	
--- src/numerical_music/stereo_to_mono.m
+++ src/numerical_music/stereo_to_mono.m
@@ -4,9 +4,6 @@ function mono = stereo_to_mono(stereo)
 	[n, m] = size(mono);
 	mono = eye(n, 1);
 
-	display(n);
-	display(m);
-
 	for i = 1:n
 		for j = 1:m
 			mono(i) = mono(i) + stereo(i, j);
