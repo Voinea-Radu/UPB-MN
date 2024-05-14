066ae20000568fff04b6dd7be64d13af9592042b
Voinea Radu <contact@voinearadu.com>
2024-05-15 00:45:10 +0300

    Implemented recommendations. Timing out on bigger tests 84/100

--- src/recommendations/recommendations.m
+++ src/recommendations/recommendations.m
@@ -1,3 +1,20 @@
-function recoms = recommendations(path, liked_theme, num_recoms, min_reviews, num_features)
-  # TODO: Get the best `num_recoms` recommandations similar with 'liked_theme'.
+function recoms = recommendations(path, liked_theme_index, num_recoms, min_reviews, num_features)
+  mat = read_mat(path);
+  mat = preprocess(mat, min_reviews);
+  
+  [U, S, V] = svds(mat, num_features);
+  liked_theme = V(liked_theme_index, :);
+
+  similarities = zeros(size(V, 1), 1);
+
+  for i = 1:size(V, 1)
+    current_v = V(i, :);
+
+    similarities(i) = cosine_similarity(current_v, liked_theme);
+  end
+
+  [sorted_similarities, indices] = sort(similarities, 'descend');
+
+  recoms = indices(2 : num_recoms+1);
+  recoms = recoms';
 end
